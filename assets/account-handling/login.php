6f8898556a12465ee652749bb0a1c24ef4e8be08
BENZOOgataga <contact@benzoogataga.com>
2025-04-02 12:37:40 +0200

    FIX: uniformisation ReverseH4ck

--- assets/account-handling/login.php
+++ assets/account-handling/login.php
@@ -46,7 +46,7 @@ if (!$alreadyLoggedIn && $_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion - E-commerce</title>
+    <title>Connexion - ReverseH4ck</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/style.css">
--- assets/website/contact.php
+++ assets/website/contact.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact - E-commerce</title>
+    <title>Contact - ReverseH4ck</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/style.css">
