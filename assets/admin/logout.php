ac1b9135f520386efc5a5c01158e1697b29003a9
smechrafi <s.mechrafi94@gmail.com>
2025-04-07 00:15:25 +0200

    commentaires logout.php

--- assets/admin/logout.php
+++ assets/admin/logout.php
@@ -2,17 +2,17 @@
 
 session_start();
 
-// Suppression des variables de session
+// suppression variables de session
 $_SESSION = array();
 
-// Destruction du cookie de session
+// destruction cookie session
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time() - 3600, '/');
 }
 
-// Destruction de la session
+// destruction session
 session_destroy();
 
-// Redirection vers la page de connexion
+// redirection page de connexion
 header('Location: ../account-handling/login.php');
 exit;
